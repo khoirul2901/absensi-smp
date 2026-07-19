3ea9694458b9cb8d1407a6afeb18e2111ca50f1a
khoirul2901 <khoirul2901@gmail.com>
2026-07-20 00:54:17 +0700

    IdCard.tsx

--- src/components/IdCard.tsx
+++ src/components/IdCard.tsx
@@ -110,9 +110,9 @@ export const IdCard: React.FC<IdCardProps> = ({ item, kategori, side = "both" })
         {/* Signature Absolute */}
         <div className="absolute bottom-2 right-3 text-center text-[7px] text-slate-300 font-medium z-30">
           <p>Sendang Mulyo, {today}</p>
-          <div className="mt-0.5 flex justify-center items-center h-[20px]">
+          <div className="mt-0.5 flex justify-center items-center h-[24px]">
             {signatureUrl ? (
-              <img src={signatureUrl} alt="Tanda Tangan" className="h-[20px] object-contain invert brightness-0 opacity-80" referrerPolicy="no-referrer" crossOrigin="anonymous" />
+              <img src={signatureUrl} alt="Tanda Tangan" className="h-[50px] object-contain invert brightness-0 opacity-80" referrerPolicy="no-referrer" crossOrigin="anonymous" />
             ) : null}
           </div>
           <div className="border-t border-slate-500 pt-[2px] w-[80px] mx-auto text-[7px] font-bold mt-1 text-slate-200">
@@ -251,7 +251,7 @@ export const IdCard: React.FC<IdCardProps> = ({ item, kategori, side = "both" })
             <p>Kepala Sekolah</p>
             <div className="mt-1 flex justify-center items-center h-[24px]">
               {signatureUrl ? (
-                <img src={signatureUrl} alt="Tanda Tangan" className="h-[24px] object-contain" referrerPolicy="no-referrer" crossOrigin="anonymous" />
+                <img src={signatureUrl} alt="Tanda Tangan" className="h-[50px] object-contain" referrerPolicy="no-referrer" crossOrigin="anonymous" />
               ) : null}
             </div>
             <div className="border-t border-gray-800 pt-[2px] w-[80px] mx-auto text-[7px] font-bold">
